acbd60fb7aac803b6d35e98daf2b0e987f13ff8d
nabfrew <nabfrew@hotmail.com>
2022-08-03 23:05:18 +0200

    strip back unnecessary complexity

--- reflector.gd
+++ reflector.gd
@@ -1,7 +1,7 @@
 @tool
 extends Sprite2D
 
-const MAX_SIZE : float = 5
+const MAX_SIZE : float = 2
 const OCCUPIED := Color(0,1,0,0.5)
 const EMPTY := Color(0,0,0,0.01)
 
@@ -66,73 +66,44 @@ func get_scaled_texture_reflection(sprite_path : String) -> Dictionary:
 	image.fill(EMPTY)
 
 	for i in range(image.get_size().x):
-		var top : Array[int] = []
-		var bottom : Array[int] = []
+		var top : int = -1
+		var bottom : int = 0
 		for j in range(image.get_size().y):
 			var source_pixel_coordinates := Vector2i(int(round(float(i) / source_scale.x)), int(round(float(j) / source_scale.y)))
 			
 			if source_pixel_coordinates.x < 0 or source_pixel_coordinates.x >= source_size.x:
 				continue
 			if source_pixel_coordinates.y < 0:
-				image.set_pixel(i, j, Color.GREEN) 
-				continue
-
-			if (true):
-				image.set_pixel(i, j, get_pixel_type(source_pixel_coordinates, source_image, top, bottom, j))
 				continue
 				
 			var pixel_color : Color = EMPTY
 			if source_pixel_coordinates.y >= source_size.y:
-				pixel_color = Color(0,1,1,0)
-			pixel_color = source_image.get_pixelv(source_pixel_coordinates)
-			if pixel_color.a == 1: 
-				if top.is_empty():
-					top.append(j)
+				pixel_color = Color(0,1,1,0) # transparent non-empty
+			else:
+				pixel_color = source_image.get_pixelv(source_pixel_coordinates)
+			
+			if pixel_color.a > 0 and top < 0: 
+				top = j
+			if pixel_color.a == 1:
+				bottom = j
+		
+		if top == -1 or bottom == 0:
+			continue
+		var reflection_range := bottom - top
+		for j in range(image.get_size().y):
+			if j < top:
+				image.set_pixel(i, j, EMPTY)
+			elif  j < bottom:
 				image.set_pixel(i, j, OCCUPIED)
-			if pixel_color.a < 1:
-				if bottom.size() < top.size(): # top value is not paired with a bottom.
-					bottom.append(j)
-				if bottom.size() == top.size():
-					if within_reflection_range(j, top, bottom):
-						image.set_pixel(i, j, coordinates_to_color(float(j), float(bottom[-1])))
+			elif j - bottom < reflection_range: 
+				image.set_pixel(i, j, coordinates_to_color(float(j), float(bottom)))
 					
 		return_dict.image = image
 	return return_dict
 
-func get_pixel_type(source_pixel_coordinates : Vector2i, source_image : Image, top : Array, bottom : Array, j : int) -> Color:
-	# image size is 2x source size. pixel can be assumed empty, but must work out if part of reflection or not.
-	var pixel_color : Color 
-	if source_pixel_coordinates.y >= source_image.get_size().y:
-		pixel_color = Color(0,1,1,0) # todo
-	else:
-		pixel_color = source_image.get_pixelv(source_pixel_coordinates)
-	
-	if pixel_color.a == 1: # part of a feature
-		if top.is_empty() or top.size() == bottom.size(): # top of a new feature
-			top.append(j)
-			return TOP_D
-		return OCCUPIED_D
-	if pixel_color.a < 1: # not part of a feature
-		if bottom.size() < top.size(): # top value is not paired with a bottom -> this *is* the feature bottom.
-			bottom.append(j)
-			return BOTTOM_D
-		if bottom.size() == top.size():
-			if within_reflection_range(j, top, bottom):
-				return REFLECTING_D
-	return UNREFLECTING_D
-
 func coordinates_to_color(j : float, bottom : float) -> Color:
 	var red_channel : float = (j - bottom) / color_scale
 	return Color(red_channel, 0 , 0 , 1)
-
-func within_reflection_range(j : int, top : Array[int], bottom : Array[int]) -> bool:
-	if top.is_empty():
-		return false
-	if j >= 2 * bottom[-1] - top[-1]:
-		top.pop_back()
-		bottom.pop_back()
-		return within_reflection_range(j, top, bottom) 
-	return true
 
 func init_reflection_with_horizon() -> Image:
 	var image : Image = Image.new()
@@ -144,7 +115,7 @@ func init_reflection_with_horizon() -> Image:
 		h = 0
 	d = position.y + h - HORIZON
 	for y in range(h, image.get_size().y):
-		var c = Color(float(d)/ color_scale, 0, 0, 1)
+		var c = Color(0, float(d)/ color_scale, 0, 1)
 		for x in range(image.get_size().x):
 			image.set_pixel(x, y, c)
 		d = d + 1.0
